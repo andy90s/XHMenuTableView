42f69767cd214929498285dce49082404046848a
梁先华 <909901234@qq.com>
2016-06-01 16:06:44 +0800

    xhliang

---
+++ XHMenuTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHMenuTableView
+//
+//  Created by liangxianhua on 16/6/1.
+//  Copyright © 2016年 xhliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHMenuTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XHMenuTableView
+//
+//  Created by liangxianhua on 16/6/1.
+//  Copyright © 2016年 xhliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
